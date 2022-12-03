c07d2d1b0b55cf47f36633fa8ea4bd8b5ac4ba05
GLORIA DOTSE <gloria_dotse@GLORIAs-MacBook-Air.local>
2022-12-03 12:41:07 +0000

    lists

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_reveresd_list_integer(my_list=[]):
+	for y in reversed(my_list):
+		print("(:d\n".format(y), end='')
---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,5 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+def print_list_integer(my_list={}):
+der element _ar(my_list, idx):
