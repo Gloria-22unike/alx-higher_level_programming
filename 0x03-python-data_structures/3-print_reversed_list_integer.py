e8e03941e38b62a06f09b9c63cb2a35d2a9dff51
GLORIA DOTSE <gloria_dotse@GLORIAs-MacBook-Air.local>
2022-12-03 13:43:31 +0000

    in reversed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,5 @@ def print_reveresd_list_integer(my_list=[]):
 	if not my_list:
 		pass
 	else:
-	for y in reversed(my_list):
-		print("(:d\n".format(y), end='')
+		for y in reversed(my_list):
+			print("(:d\n".format(y), end='')
