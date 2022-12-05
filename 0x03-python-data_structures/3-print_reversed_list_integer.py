514df616b66b17092aff7772b4281abd0310627a
GLORIA DOTSE <gloria_dotse@GLORIAs-MacBook-Air.local>
2022-12-05 02:29:53 +0000

    in reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def print_reveresd_list_integer(my_list=[]):
-	if not my_list:
-		pass
-	else:
-		for y in reversed(my_list):
-			print("(:d\n".format(y), end='')
+def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        pass
+    else:
+        for i in reversed(my_list):
+            print("{:d}\n".format(i), end='')
