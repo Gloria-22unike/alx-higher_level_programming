a9ae946f0cb0b3f6314973cfb648b321bdc2de12
GLORIA DOTSE <gloria_dotse@GLORIAs-MacBook-Air.local>
2022-12-21 02:21:20 +0000

    size square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+
+"""Define a class Square."""
+
+
 class Square:
-    """Represents a square.
+    """Represents a square."""
     Private instance attribute: size.
     Instantiation with size (no type/value verification).
     """
